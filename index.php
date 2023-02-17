900fb1d87dada7841f50b267eead8be03847744e
Aarti <aartic113@gmail.com>
2023-02-17 20:21:20 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+    <!-- <link rel="stylesheet" href="reset.css" /> -->
+    <title>Aarti Chandnani</title>
+  </head>
+  <body>
+    <nav class="nav">
+      <div class="container">
+        <h1 class="logo"><a href="/index.html">Aarti Chandnani</a></h1>
+        <ul>
+          <li><a href="#about-me" class="current">Home</a></li>
+          <!-- <li><a href="#hobbies">Hobbies</a></li>
+          <li><a href="#my-cv">My CV</a></li>
+          <li><a href="#links">Links</a></li> -->
+        </ul>
+      </div>
+    </nav>
+
+    <div class="hero">
+      <div class="container">
+        <!-- <h1>First Year Student at QMUL</h1> -->
+        <p>Studying BSc Computer Science</p>
+      </div>
+    </div>
+    
+
+    <section id="hobbies">
+      <section class="container content">
+        <h3>Hobbies</h3>
+        <ul>
+          <p>I enjoy the following activities:</p>
+          <li>Reading</li>
+          <li>Running</li>
+          <li>Cycling</li>
+          <li>Yoga</li>
+          <li>Coding</li>
+          <li>Listening to Music</li>
+          <li>Watching shows on Netflix and Youtube</li>
+        </ul>
+      </section>
+    </section>
+
+   <br>
+
+<!--     
+    <hr />
+    <footer>
+      <p>Copyright <span>&copy;</span> 2023 Aarti Chandnani</p>
+    </footer> -->
+
+    <script src="js/script.js"></script>
+  </body>
+</html>
